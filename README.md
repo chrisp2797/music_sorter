f5751431ca3c320e86a37719d8e888e7a2b78f03
Chris K. Panorios <cpanorios@gmail.com>
2020-04-23 00:14:26 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#A SIMPLE MUSIC SORTING PYTHON SCRIPT
+
+This is a small python script that sorts unsorted mp3 files based on the Artist and the Album.
+It Reads the metadata of the mp3 for the artist's and album's name.
+For the script to work the user must have Python3 and install the eyed3 library, by typing "pip install eyed3".
---
+++ music_sorter.py
@@ -0,0 +1,67 @@
+import os
+import shutil
+import eyed3
+import sys
+#block and enable print because we dont want the eyed3 to print
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+#Windows illegal directory names,used to create a valid Artist and/or Album directory
+def check_illegal_name(s):
+    illegals="/\:*<>|."
+    for i in illegals:
+        if i in s:
+            return True
+    return False
+
+def strip_illegals(s):
+    new_s=""
+    illegals="/\:*<>|."
+    for i in s:
+        if i not in illegals:
+            new_s+=i
+    return new_s
+    
+    
+def sort_music_dir(direc):
+    blockPrint()
+    try:
+        songs=os.listdir(direc)
+    except:
+        enablePrint()
+        direc=input("Please give a valid directory path.:")
+        sort_music_dir(direc)
+        return
+    for i in songs:
+        try:
+            song=eyed3.load(direc+'/'+i)
+            artist=song.tag.album_artist
+            album=song.tag.album
+        except:
+            continue
+        if not artist:
+            artist="Various Artists"
+        if not album:
+            album=""
+        if check_illegal_name(artist):
+            artist=strip_illegals(artist)
+        if check_illegal_name(album):
+            album=strip_illegals(album)
+        if not os.path.exists(direc+'/'+artist):
+            os.mkdir(direc+'/'+artist)
+        if not os.path.exists(direc+'/'+artist+'/'+album):
+            os.mkdir(direc+'/'+artist+'/'+album)
+        shutil.move(direc+'/'+i,direc+'/'+artist+'/'+album)
+    enablePrint()
+    return
+def main():
+    org_directory=input("Give full path of the Directory you want to sort:")
+    sort_music_dir(org_directory)
+    print("",end="\r")
+    input("Operation Complete.\nPress enter to exit.")
+
+if __name__=='__main__':
+    main()
